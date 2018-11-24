7c8e19780db1262e776dc1286993f896193f92f0
gengelke <gordon.e@email.de>
2018-11-24 20:36:30 +0100

    adapted to mojave

---
+++ macos/firefox.sh
@@ -0,0 +1,14 @@
+#https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data
+#https://support.mozilla.org/de/kb/firefox-profile-sichern-und-wiederherstellen
+#Macintosh HD⁩ ▸ ⁨Users⁩ ▸ ⁨gengelke⁩ ▸ ⁨Library⁩ ▸ ⁨Application Support⁩ ▸ ⁨Firefox⁩
+#places.sqlite
+#prefs.js
+#favicons.sqlite
+#permissions.sqlite
+#key3.db
+#content-prefs.sqlite
+#extensions.json
+#extensions
+#formhistory.sqlite
+#cookies.sqlite
+#key4.db
