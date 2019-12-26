14e1bf6c3bc160647d0496d330730e6a5372882a
gengelke <garbage@email.de>
2019-12-26 19:38:45 +0100

    removed slack and textual from dock

--- macos/macsetup.sh
+++ macos/macsetup.sh
@@ -695,9 +695,9 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/XnViewMP.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Textual.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+#defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Textual.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Slack.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+#defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Slack.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 
 # Show only open applications in the Dock
 #defaults write com.apple.dock static-only -bool true
