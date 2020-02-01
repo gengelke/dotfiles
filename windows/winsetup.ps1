e6857da89889d4805a2e7af84dba143013aa71f9
Gordon Engelke <36102876+gengelke@users.noreply.github.com>
2020-02-01 15:13:06 +0100

    disable lid close power off

--- windows/winsetup.ps1
+++ windows/winsetup.ps1
@@ -69,6 +69,15 @@ else {
 
 
 #=================================#
+# Power saving settings           #
+#=================================#
+
+Write-Host "`n=> Disabling power off when lid is closed..."
+powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+
+
+#=================================#
 # UI/UX modifications             #
 #=================================#
 
