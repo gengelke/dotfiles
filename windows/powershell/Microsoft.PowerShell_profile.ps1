9d93c77d583b75ad3c6abb1a68486e868b5eefe3
Gordon Engelke <36102876+gengelke@users.noreply.github.com>
2021-06-19 21:55:25 +0200

    Update Microsoft.PowerShell_profile.ps1

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,7 @@
 # Powershell:        ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 # Azure Cloud Shell: ~/.config/PowerShell/Microsoft.PowerShell_profile.ps1
 
+clear
 write-host "`n-{ gengelke posh profile }-";
 
 #$host.UI.RawUI.ForegroundColor = "Gray"
